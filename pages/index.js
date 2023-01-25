c870ff0fe2e334527cce46f2683832c6a8d60c3a
SoloRatRacer97 <todd.j.anderson.1997@gmail.com>
2023-01-25 12:52:44 -0800

    Initiaial commit

--- pages/index.js
+++ pages/index.js
@@ -1,33 +1,37 @@
-import { Fragment } from 'react';
-import Head from 'next/head';
+import fs from 'fs/promises';
+import path from 'path';
 
-import FeaturedPosts from '../components/home-page/featured-posts';
-import Hero from '../components/home-page/hero';
-import { getFeaturedPosts } from '../lib/posts-util';
 
 function HomePage(props) {
+
+  const { products } = props;
+
   return (
-    <Fragment>
-      <Head>
-        <title>Max' Blog</title>
-        <meta
-          name='description'
-          content='I post about programming and web development.'
-        />
-      </Head>
-      <Hero />
-      <FeaturedPosts posts={props.posts} />
-    </Fragment>
+    <ul>
+      {products.map((product) => (
+        <li key={product.id}>{product.title}</li>
+      ))}
+    </ul>
   );
 }
 
-export function getStaticProps() {
-  const featuredPosts = getFeaturedPosts();
+// For Next.js apps, this code below is ran first, then the code above is ran to generate the JSX
+export async function getStaticProps() {
+
+  // This is saying we want the path to start at the main directory, then dive into the data folder, then grab the dummy-backend json file
+  const filePath = path.join(process.cwd(), 'data', 'dummy-backend.json');
+  // Then we read this file on the file path to that dummy-backend data. 
+  const jsonData = await fs.readFile(filePath);
+  // Then we parse that JSON data so we can use it. 
+  const data = JSON.parse(jsonData);
+
 
   return {
     props: {
-      posts: featuredPosts,
+      products: data.products
     },
+    // This is where we introduce ISR for regenerating the page every X number of seconds
+    revalidate: 60
   };
 }
 
